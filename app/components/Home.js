b8d13c21542c9d24c61e9d7e73fc6ea99983c9de
Artur Świątek-Brzeziński <artil87@gmail.com>
2017-01-01 18:05:06 +0100

    Started work on displaying JSON data.

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,13 +1,16 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var TechniquesList = require('../components/TechniquesList');
 
 var Home = React.createClass({
-  render: function (){
-    return (
-      <h2 className="text-center">
-        Search by Github user name
-      </h2>
-    )
-  }
+    render: function() {
+        return (
+          <div>
+            <h2 className="text-center">List of all techniques</h2>
+            <TechniquesList />
+          </div>
+        )
+    }
 });
 
 module.exports = Home;
--- app/config/routes.js
+++ app/config/routes.js
@@ -1,14 +1,14 @@
 var React = require('react');
 var Main = require('../components/Main');
 var Home = require('../components/Home');
-var Profile = require('../components/Profile');
+var Details = require('../components/Details');
 var Router = require('react-router');
 var Route = Router.Route;
 var IndexRoute = Router.IndexRoute;
 
 module.exports = (
   <Route path="/" component={Main}>
-    <Route path="technique/:id" component={Profile} />
+    <Route path="technique/:id" component={Details} />
     <IndexRoute component={Home} />
   </Route>
 );
